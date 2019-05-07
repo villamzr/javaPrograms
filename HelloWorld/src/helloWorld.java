3b0b5318152dde3fc5a2ad28ae2c4266c58c86fb
villamzr <villamzar@gmail.com>
2019-05-07 00:19:41 -0500

    mi primer Hola Mundo en java con el IDE Intellij

---
+++ HelloWorld/src/helloWorld.java
@@ -0,0 +1,11 @@
+public class helloWorld{
+
+    public static String saludo = "Hola Mundo!";
+
+    public static void main(String[] args){
+        helloWorld hw = new helloWorld();
+
+        System.out.println(hw.saludo);
+    }
+
+}
